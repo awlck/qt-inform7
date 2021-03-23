1ad645854e986003150f3594d1f7a78bba603641
Adrian Welcker <ardi@diepixelecke.de>
2021-03-24 00:08:56 +0100

    Add config option for paths

--- src/config.cpp
+++ src/config.cpp
@@ -5,27 +5,33 @@
 #include "config.h"
 
 #include <QtCore/QCoreApplication>
+#include <QtCore/QSettings>
 #include <QtCore/QDir>
 #include <QtCore/QFile>
 
 Config::Config() {
 	appDir = QCoreApplication::applicationDirPath();
-	{
-		QDir dir(appDir);
+	QSettings settings;
+	if (settings.value("paths/auto", true).toBool()) {
+		{
+			QDir dir(appDir);
 #ifdef Q_OS_WIN
-		if (QFile(dir.absoluteFilePath("ni.exe")).exists()) compilersDir = appDir;
-		else if (dir.cd("Compilers") && QFile(dir.absoluteFilePath("ni.exe")).exists()) compilersDir = dir.absolutePath();
-		else compilersDir = "COMPILERS_NOTFOUND";
+			if (QFile(dir.absoluteFilePath("ni.exe")).exists()) compilersDir = appDir;
+			else if (dir.cd("Compilers") && QFile(dir.absoluteFilePath("ni.exe")).exists()) compilersDir = dir.absolutePath();
+			else compilersDir = "COMPILERS_NOTFOUND";
 #else
-		if (QFile(dir.absoluteFilePath("ni")).exists()) compilersDir = appDir;
-		else if (dir.cd("Compilers") && QFile(dir.absoluteFilePath("ni")).exists()) compilersDir = dir.absolutePath();
-		else compilersDir = "COMPILERS_NOTFOUND";
+			if (QFile(dir.absoluteFilePath("ni")).exists()) compilersDir = appDir;
+			else if (dir.cd("Compilers") && QFile(dir.absoluteFilePath("ni")).exists()) compilersDir = dir.absolutePath();
+			else compilersDir = "COMPILERS_NOTFOUND";
 #endif
-	}
-	{
-		// QDir dir(appDir);
-		QDir dir("C:/Program Files/Inform 7");
-		if (dir.cd("Documentation")) dataDir = dir.absolutePath();
-		else dataDir = "DATA_NOTFOUND";
+		}
+		{
+			QDir dir(appDir);
+			if (dir.cd("Documentation")) dataDir = dir.absolutePath();
+			else dataDir = "DATA_NOTFOUND";
+		}
+	} else {
+		compilersDir = settings.value("paths/compilers", "COMPILERS_NOTFOUND").toString();
+		dataDir = settings.value("paths/data", "DATA_NOTFOUND").toString();
 	}
 }
